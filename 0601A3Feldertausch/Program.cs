8f13f089b28e698f4789a8a3fe69046363b77e10
frank paulsen <frobnic+ita@gmail.com>
2019-08-08 12:29:30 +0200

    code inserted

--- 0601A3Feldertausch/Program.cs
+++ 0601A3Feldertausch/Program.cs
@@ -10,6 +10,27 @@ namespace _0601A3Feldertausch
     {
         static void Main(string[] args)
         {
+            sbyte[] feld = new sbyte[10];
+            sbyte x;
+            Random zuf = new Random();
+
+            for (int i = 0; i < 10; i++)
+                feld[i] = (sbyte)(zuf.Next(1, 33));
+
+            foreach (int n in feld)
+                Console.Write($"{n} ");
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                x = feld[i];
+                feld[i] = feld[9 - i];
+                feld[9 - i] = x;
+            }
+            foreach (int n in feld)
+                Console.Write($"{n} ");
+            Console.WriteLine();
+
         }
     }
 }
