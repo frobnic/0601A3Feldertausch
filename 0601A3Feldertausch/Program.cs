085ea73029e910e3edc719997000b2dff4d2aa39
frank paulsen <frobnic+ita@gmail.com>
2019-08-08 12:09:49 +0200

    Projektdateien hinzufügen.

---
+++ 0601A3Feldertausch/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0601A3Feldertausch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
